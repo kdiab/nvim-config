18baf0aeaf0f5ed0b8a5f71dd22defd87b09eda6
Diab <khalid@diab.cc>
2024-01-03 19:14:15 -0500

    auto completion mappings

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -37,9 +37,9 @@ cmp.setup({
 	},
 	formatting = lsp_zero.cmp_format(),
 	mapping = cmp.mapping.preset.insert({
-		['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-		['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-		['<C-y>'] = cmp.mapping.confirm({ select = true }),
+		['<C-]>'] = cmp.mapping.select_prev_item(cmp_select),
+		['<C-[>'] = cmp.mapping.select_next_item(cmp_select),
+		['<C-p>'] = cmp.mapping.confirm({ select = true }),
 		['<C-Space>'] = cmp.mapping.complete(),
 	}),
 })
